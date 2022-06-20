ef0ac2f252410cecc8ad107305f7e77437c76d09
Ruslan Aliev <raliev@mirantis.com>
2022-06-20 16:50:24 -0500

    Bump helm version from 3.6.3 to 3.8.2
    Also bump k8s dependency to v1.23.
    
    Signed-off-by: Ruslan Aliev <raliev@mirantis.com>
    Change-Id: I188788a41419184cc3dd9172f54ab3a290cabe83

--- setup.py
+++ setup.py
@@ -5,4 +5,8 @@ try:
 except ImportError:
     pass
 
-setuptools.setup(setup_requires=['pbr>=2.0.0'], pbr=True)
+setuptools.setup(
+    package_data={'armada': ['schemas/*.yaml']},
+    include_package_data=True,
+    setup_requires=['pbr>=2.0.0'],
+    pbr=True)
